8609a7478952b7f6c79ef70ebb5ab253bc5c53c9
James Florez <OssRezz.13@gmail.com>
2023-08-07 21:20:12 -0500

    Agregando funcionalidad de incrementar, descontar items del carrito, separecion del codigo en componentes

--- guitar-vue/src/components/Header.vue
+++ guitar-vue/src/components/Header.vue
@@ -1,7 +1,96 @@
-<script setup></script>
+<script setup>
+const props = defineProps({
+    carrito: {
+        type: Array,
+        required: true
+    },
+    guitarra: {
+        type: Object,
+        required: true
+    }
+});
+
+defineEmits(['aumentar-carrito', 'descontar-carrito', 'eliminar-Carrito', 'agregar-Guitarra']);
+</script>
 
 <template>
-    <div></div>
-</template>
+    <header class="py-5 header">
+        <div class="container-xl">
+            <div class="row justify-content-center justify-content-md-between">
+                <div class="col-8 col-md-3">
+                    <a href="index.html">
+                        <img class="img-fluid" src="/img/logo.svg" alt="imagen logo">
+                    </a>
+                </div>
+                <nav class="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+                    <div class="carrito">
+                        <img class="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
 
-<style></style>
+                        <div id="carrito" class="bg-white p-3">
+                            <p v-if="carrito.length === 0" class="text-center">
+                                El carrito esta vació
+                            </p>
+                            <div v-else>
+                                <table v-if="carrito.length > 0" class="w-100 table">
+                                    <thead>
+                                        <tr>
+                                            <th>Imagen</th>
+                                            <th>Nombre</th>
+                                            <th>Precio</th>
+                                            <th>Cantidad</th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr v-for="guitarra in carrito">
+                                            <td>
+                                                <img class="img-fluid" :src="`/img/${guitarra.imagen}.jpg`"
+                                                    :alt="`imagen guitarra ${guitarra.nombre}`">
+                                            </td>
+                                            <td>{{ guitarra.nombre }}</td>
+                                            <td class="fw-bold">
+                                                ${{ guitarra.precio }}
+                                            </td>
+                                            <td class="flex align-items-start gap-4">
+                                                <button type="button" class="btn btn-dark"
+                                                    @click="$emit('descontar-Carrito', guitarra.id)">
+                                                    -
+                                                </button>
+                                                {{ guitarra.cantidad }}
+                                                <button type="button" class="btn btn-dark"
+                                                    @click="$emit('aumentar-Carrito', guitarra.id)">
+                                                    +
+                                                </button>
+                                            </td>
+                                            <td>
+                                                <button class="btn btn-danger" type="button"
+                                                    @click="$emit('eliminar-Carrito', guitarra.id)">
+                                                    X
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+
+                                <p class="text-end">Total pagar: <span class="fw-bold">$899</span></p>
+                                <button class="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div><!--.row-->
+
+            <div class="row mt-5">
+                <div class="col-md-6 text-center text-md-start pt-5">
+                    <h1 class="display-2 fw-bold">Modelo {{ guitarra.nombre }}</h1>
+                    <p class="mt-5 fs-5 text-white"> {{ guitarra.descripcion }}</p>
+                    <p class="text-primary fs-1 fw-black">$ {{ guitarra.precio }}</p>
+                    <button type="button" class="btn fs-4 bg-primary text-white py-2 px-5"
+                        @click="$emit('agregar-Guitarra', guitarra)">Agregar al Carrito</button>
+                </div>
+            </div>
+        </div>
+
+        <img class="header-guitarra" src="/img/header_guitarra.png" alt="imagen header">
+    </header>
+</template>
